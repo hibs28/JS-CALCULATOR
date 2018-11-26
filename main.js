4c3a3df78957ddb2c95f2e2a80598cd516a5cc61
Tech-A06 <hibah28@live.co.uk>
2018-11-26 23:43:06 +0000

    basic calculator and advanced done

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-// The following might help:
+while (operator !== "q") {
+  var num1Str = prompt("enter your first number");
+  var num2Str = prompt("enter your second number");
+  var num1 = parseFloat(num1Str);
+  var num2 = parseFloat(num2Str);
+  var operator = prompt("operator + - / *");
+  var result;
 
-// * if
-// * else
-// * prompt
-//     * console.log
-//     * prompt returns a string even if you type in a number.You might need to change it to a number
-
-// #### Feature 1
-
-var calc = prompt("please enter your equation");
-
-// User should be able to enter two values to add together.The sum of the two values should be shown to the user.This should work for all numbers including decimals.
-
-
-// #### Feature 2
-
-// User should be able to enter two values then choose between adding, diving, subtracting, or multiplying.The answer should be shown to the user.
-
-// #### Feature 3
-
-// Calculator should choose addition if the user leaves it blank.
-
-// #### Bonus
-
-// Research the while loop.
-
-// User should be able to make more than one calculation or choose to quit the calculator
+  switch (operator) {
+    case "/":
+      result = num1 / num2;
+      alert(result);
+      break;
+    case "*":
+      result = num1 * num2;
+      alert(result);
+      break;
+    case "":
+      result = num1 - num2;
+      alert(result);
+      break;
+    case "+":
+      result = num1 + num2;
+      alert(result);
+      break;
+    case "q":
+      window.close();
+      break;
+    default:
+      result = num1 + num2;
+      alert(result);
+      break;
+  }
+}
